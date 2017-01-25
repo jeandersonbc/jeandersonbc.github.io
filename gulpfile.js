991b8bf7326ada131b1e7881116ec1726629e653
Jeanderson Candido <jeandersonbc@gmail.com>
2017-01-24 23:36:00 -0300

    Fixes #2 - Added task to concat and minify sources

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,27 @@
 var gulp = require('gulp')
+var usemin = require('gulp-usemin')
+var minifyHtml = require('gulp-minify-html')
+var cleanCSS = require('gulp-clean-css')
 var del = require('del')
 
 var paths = {
-    src: "./src",
-    build: "./target"
+    src: './src',
+    build: './target'
 };
 
 gulp.task('clean', function() {
     del([paths.build]);
 });
 
-gulp.task('copy', ['clean'], function() {
-    return gulp.src(paths.src + "/*")
+gulp.task('usemin', ['clean'], function() {
+    return gulp.src(paths.src + '/index.html')
+        .pipe(usemin({
+            html: [minifyHtml({empty: true})],
+            css: ['concat', cleanCSS()],
+            js: ['concat']
+        }))
         .pipe(gulp.dest(paths.build));
 });
 
-gulp.task('build', ['copy'])
-
+gulp.task('build', ['usemin'])
 gulp.task('default', ['build']);
