46fc6cc476e0a3ef5b26cffef97c1ec459df5413
Jeanderson Candido <jeandersonbc@gmail.com>
2017-01-24 21:51:46 -0300

    Fixes #1 - Initial gulpfile.js version
    Created gulpfile with the following tasks:
    - build
    - clean
    - copy

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
 var gulp = require('gulp')
+var del = require('del')
 
-gulp.task('default', function() {
-    // default task
+var paths = {
+    src: "./src",
+    build: "./target"
+};
+
+gulp.task('clean', function() {
+    del([paths.build]);
 });
+
+gulp.task('copy', ['clean'], function() {
+    return gulp.src(paths.src + "/*")
+        .pipe(gulp.dest(paths.build));
+});
+
+gulp.task('build', ['copy'])
+
+gulp.task('default', ['build']);
