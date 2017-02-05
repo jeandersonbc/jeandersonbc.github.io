833d5db928b68c2df99dcbfa4c02ddb4b6f2ff57
Jeanderson Candido <jeandersonbc@gmail.com>
2017-02-05 18:12:03 -0300

    Added Font-Awesome

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,8 @@ var gulp = require('gulp'),
     usemin = require('gulp-usemin'),
     minifyHtml = require('gulp-minify-html'),
     cleanCSS = require('gulp-clean-css'),
-    webserver = require('gulp-webserver')
+    webserver = require('gulp-webserver'),
+    replace = require('gulp-replace')
 
 var path = {
     src: './src',
@@ -14,28 +15,36 @@ gulp.task('clean', function() {
     del([path.build]);
 });
 
-gulp.task('usemin', ['clean'], function() {
+gulp.task('usemin', function() {
     return gulp.src(path.src + '/index.html')
         .pipe(usemin({
             html: [minifyHtml({empty: true})],
             css: ['concat', cleanCSS()],
             js: ['concat']
         }))
+        .pipe(replace('#CURRENTDATE#', (new Date()).toDateString()))
         .pipe(gulp.dest(path.build));
 });
 
-gulp.task('assets', ['clean'], function() {
+gulp.task('assets', function() {
     return gulp.src(path.src + '/assets/*', {base: path.src})
         .pipe(gulp.dest(path.build));
 });
 
-gulp.task('build', ['assets', 'usemin']);
-
-gulp.task('webserver', function() {
-    gulp.src(path.build)
-        .pipe(webserver({
-            livereload: {enable: true}
-        }));
+gulp.task('fonts', function() {
+    return gulp.src(path.src + '/bower_components/font-awesome/fonts/fontawesome-webfont.*')
+        .pipe(gulp.dest(path.build + "/fonts/"));
 });
 
-gulp.task('default', ['build']);
+gulp.task('build', function() {
+    gulp.start('clean');
+    gulp.start('usemin', 'assets', 'fonts');
+});
+
+gulp.task('webserver', function() {
+    gulp.src(path.build).pipe(webserver({
+        livereload: {enable: true}
+    }));
+});
+
+gulp.task('default', ['build'])
